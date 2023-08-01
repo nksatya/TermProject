837c4399e20b086ceedafab94892059ef883d0b8
Karthik Saride <32055885+karthikkraze@users.noreply.github.com>
2023-08-01 19:15:25 -0400

    Changes done for saving the details in JSON format in localstorage

--- JS/register.js
+++ JS/register.js
@@ -1,13 +1,31 @@
 function validateForm() {
+
+    const registrationForm = document.getElementsByClassName('register-form');
+
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    if( email != null && password != null ) {
+    const registrationDetails = {
+        fname : fname,
+        lname : lname,
+        email : email,
+        password : password,
+    };
+
+    const registerationJSON = JSON.stringify(registrationDetails);
+
+    localStorage.setItem("registrationDetails", registerationJSON);
+
+    if( registrationDetails != null ) {
 
         document.getElementById("registerMessage").style.display = "block";
 
         setTimeout(function(){
             window.location.href="login.html";
         },3000);
+
+        // registrationForm.reset();
     }
 }
