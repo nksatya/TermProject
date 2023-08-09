60ae2f174141256e862fb1195d6b24ea1384d353
Karthik Saride <32055885+karthikkraze@users.noreply.github.com>
2023-08-09 17:45:38 -0400

    Implemented index db for registration page

--- JS/register.js
+++ JS/register.js
@@ -1,4 +1,43 @@
+const dbName = 'RegistrationDB';
+const storeName = 'users';
+
+// Open or create the IndexedDB database
+const request = indexedDB.open(dbName, 1);
+
+request.onerror = (event) => {
+    console.log('Error opening database:', event.target.error);
+};
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    // Create an object store to store user data
+    const objectStore = db.createObjectStore(storeName, { keyPath: 'email' });
+    objectStore.createIndex('email', 'email', { unique: true });
+};
+
+function addUserToDB(user) {
+    const db = request.result;
+    const transaction = db.transaction(storeName, 'readwrite');
+    const objectStore = transaction.objectStore(storeName);
+    objectStore.add(user);
+}
+
 function validateForm() {
+    // Your form validation logic here
+    const fname = document.getElementById('fname').value;
+    const lname = document.getElementById('lname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const user = { fname, lname, email, password };
+    addUserToDB(user);
+
+    const registerMessage = document.getElementById('registerMessage');
+    registerMessage.style.display = 'block';
+}
+
+
+/*function validateForm() {
 
     const registrationForm = document.getElementsByClassName('register-form');
 
@@ -28,4 +67,4 @@ function validateForm() {
 
         // registrationForm.reset();
     }
-}
+}*/
